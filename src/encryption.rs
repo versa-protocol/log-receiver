660c6c48e9959e45dd10ac8e953fac1ac746f2a0
Thomas Constantine Moore <tomismore@gmail.com>
2024-05-07 17:09:55 -0400

    formatting

--- src/encryption.rs
+++ src/encryption.rs
@@ -6,7 +6,6 @@ use serde::Deserialize;
 
 use crate::model::Envelope;
 
-
 pub fn decrypt_envelope<T>(envelope: &Envelope, key: &Vec<u8>) -> T
 where
     T: for<'a> Deserialize<'a>,
@@ -19,6 +18,5 @@ where
         )
         .expect("Decryption works");
     let canonical_json = String::from_utf8(decrypted).expect("Works");
-    serde_json::from_str::<T>(&canonical_json)
-        .expect("Deserialization should work")
+    serde_json::from_str::<T>(&canonical_json).expect("Deserialization should work")
 }
