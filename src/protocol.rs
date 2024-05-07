9172a9a7cbb3ab89a575a6715426fe5a9392d772
Thomas Constantine Moore <tomismore@gmail.com>
2024-05-07 17:09:37 -0400

    log seller org

--- src/protocol.rs
+++ src/protocol.rs
@@ -8,9 +8,31 @@ pub struct CheckoutRequest {
 }
 
 #[derive(Deserialize)]
-pub struct Key {
+pub struct Transaction {
     pub transaction_hash: String,
     pub decryption_key: Vec<u8>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Org {
+    pub id: String,
+    pub name: String,
+    pub slug: String,
+    pub website: String,
+    pub logo_url: Option<String>,
+    pub brand_color: Option<String>,
+    pub stock_symbol: Option<String>,
+    pub twitter: Option<String>,
+    pub isin: Option<String>,
+    pub lei: Option<String>,
+    pub naics: Option<String>,
+    pub created: i64,
+}
+
+#[derive(Deserialize)]
+pub struct Checkout {
+    pub transaction: Transaction,
+    pub seller: Option<Org>,
 }
 
 #[derive(Deserialize, Serialize)]
@@ -19,18 +41,15 @@ pub struct ReceiverPayload {
     pub envelope: Envelope,
 }
 
-
 pub async fn checkout_key(
     client_id: &str,
     client_secret: &str,
     transaction_hash: u64,
-) -> Result<Key, ()> {
+) -> Result<Checkout, ()> {
     let registry_url = std::env::var("REGISTRY_URL").unwrap_or_default();
     let credential = format!("{}:{}", client_id, client_secret);
 
-    let payload = CheckoutRequest {
-        transaction_hash,
-    };
+    let payload = CheckoutRequest { transaction_hash };
 
     let payload_json = serde_json::to_string(&payload).unwrap();
 
@@ -50,7 +69,7 @@ pub async fn checkout_key(
     };
 
     if res.status().is_success() {
-        let data: Key = match res.json().await {
+        let data: Checkout = match res.json().await {
             Ok(val) => val,
             Err(e) => {
                 info!("Failed to deserialize due to error: {}", e);
--- src/receiver.rs
+++ src/receiver.rs
@@ -1,29 +1,37 @@
-
 use crate::protocol::ReceiverPayload;
 
 pub async fn target(
     // headers: HeaderMap,
     axum::extract::Json(body): axum::extract::Json<ReceiverPayload>,
 ) -> Result<http::StatusCode, (http::StatusCode, String)> {
-
     let ReceiverPayload {
         sender_client_id,
         envelope,
     } = body;
 
-    let receiver_client_id =  std::env::var("CLIENT_ID").unwrap_or_default();
+    let receiver_client_id = std::env::var("CLIENT_ID").unwrap_or_default();
     let receiver_client_secret = std::env::var("CLIENT_SECRET").unwrap_or_default();
 
+    let checkout =
+        crate::protocol::checkout_key(&receiver_client_id, &receiver_client_secret, envelope.hash)
+            .await
+            .map_err(|_| {
+                (
+                    http::StatusCode::INTERNAL_SERVER_ERROR,
+                    "Failed to checkout key".to_string(),
+                )
+            })?;
 
-    let key = crate::protocol::checkout_key(
-        &receiver_client_id,
-        &receiver_client_secret,
-        envelope.hash,
-    ).await.map_err(|_| (http::StatusCode::INTERNAL_SERVER_ERROR, "Failed to checkout key".to_string()))?;
+    info!("Received envelope from sender: {:?}", checkout.seller);
+    let data = crate::encryption::decrypt_envelope::<crate::model::SenderReceiptHeader>(
+        &envelope,
+        &checkout.transaction.decryption_key,
+    );
 
-    let data = crate::encryption::decrypt_envelope::<crate::model::SenderReceiptHeader>(&envelope, &key.decryption_key);
-
-    info!("DATA RECEIVED FROM SENDER_CLIENT_ID={}: {:?}", sender_client_id, data);
+    info!(
+        "DATA RECEIVED FROM SENDER_CLIENT_ID={}: {:?}",
+        sender_client_id, data
+    );
 
     Ok(http::StatusCode::ACCEPTED)
 }
