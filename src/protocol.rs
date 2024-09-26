506320746c3b99e4fc1cff175fa1f8416516bf08
Thomas Constantine Moore <tomismore@gmail.com>
2024-09-26 16:30:27 -0400

    update service version to 1.3.0 to match schema version; install versa crate

--- src/protocol.rs
+++ src/protocol.rs
@@ -1,61 +1,11 @@
 use serde::{Deserialize, Serialize};
+use versa::protocol::Checkout;
 
 use crate::model::Envelope;
 
 #[derive(Serialize)]
 pub struct CheckoutRequest {
     pub receipt_id: String,
-}
-
-#[derive(Deserialize)]
-pub struct Transaction {
-    pub receipt_hash: String,
-    pub encryption_key: String,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct Org {
-    pub id: String,
-    pub name: String,
-    pub slug: String,
-    pub website: String,
-    pub logo_url: Option<String>,
-    pub brand_color: Option<String>,
-    pub stock_symbol: Option<String>,
-    pub twitter: Option<String>,
-    pub isin: Option<String>,
-    pub lei: Option<String>,
-    pub naics: Option<String>,
-    pub created: i64,
-}
-
-#[derive(Deserialize)]
-pub struct ReceiptRecord {
-    pub schema_version: String,
-    pub receipt_hash: String,
-    pub encryption_key: String,
-}
-
-#[derive(Deserialize)]
-pub struct TransactionHandles {
-    pub customer_email: Option<String>,
-    pub customer_email_domain: Option<String>,
-    pub customer_email_uhash: Option<String>,
-    pub card_last_four: Option<String>,
-    pub card_bin: Option<String>,
-    /// WARN: currently for testing purposes only — do not use
-    pub versa_client_ids: Option<Vec<String>>,
-}
-
-#[derive(Deserialize)]
-pub struct Checkout {
-    pub key: String,
-    pub receipt_id: String,
-    pub receipt_hash: String,
-    pub schema_version: String,
-    pub transaction_id: String,
-    pub sender: Option<Org>,
-    pub handles: TransactionHandles,
 }
 
 #[derive(Deserialize, Serialize)]
