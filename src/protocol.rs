e8f173383d3fbc71e220d404a465d2936b79c092
Thomas Constantine Moore <thomas@versa.org>
2024-11-08 10:05:02 -0500

    Update versa crate

--- src/protocol.rs
+++ src/protocol.rs
@@ -8,11 +8,9 @@ pub async fn checkout_key(
     receipt_id: String,
 ) -> Result<Checkout, ()> {
     let receiver_secret = std::env::var("RECEIVER_SECRET").expect("RECEIVER_SECRET must be set");
-    let registry_url = std::env::var("REGISTRY_URL").unwrap_or_default();
 
-    let versa_client = VersaClient::new(registry_url, client_id.into(), client_secret.into())
-        .receiving_client(receiver_secret)
-        .unwrap();
+    let versa_client =
+        VersaClient::new(client_id.into(), client_secret.into()).receiving_client(receiver_secret);
 
     match versa_client.checkout_key(receipt_id).await {
         Ok(checkout) => Ok(checkout),
